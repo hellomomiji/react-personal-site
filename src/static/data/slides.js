dc31726655ba4049efba6257c4c06e914c818f29
Yang Jiang <jiangyang.jnu@gmail.com>
2023-04-27 00:02:24 -0700

    modify img path

--- src/static/data/slides.js
+++ src/static/data/slides.js
@@ -2,7 +2,7 @@ const slides = [
   {
     id: 0,
     title: "NEU Hackers",
-    img: "/img/projects-neucommunity.png",
+    img: "/react-personal-site/public/img/projects-neucommunity.png",
     imgalt:"NEU Hackers project screenshot",
     link: "https://github.com/hellomomiji/NeuCommunity",
 
@@ -10,28 +10,28 @@ const slides = [
   {
     id: 1,
     title: "Personalized Imdb",
-    img: "/img/projects-imdb.png",
+    img: "/react-personal-site/public/img/projects-imdb.png",
     imgalt:"imdb project screenshot",
     link: "https://github.com/hellomomiji/personalized-imdb-app",
   },
   {
     id: 2,
     title: "Refine Dashboard App",
-    img: "/img/wip.jpg",
+    img: "/react-personal-site/public/img/wip.jpg",
     imgalt:"work in porgress",
     link: "https://github.com/hellomomiji/refine-dashboard-app",
   },
   {
     id: 3,
     title: "Chinese Blog",
-    img: "/img/projects-blog.png",
+    img: "/react-personal-site/public/img/projects-blog.png",
     imgalt:"blog project screenshot",
     link: "https://github.com/hellomomiji/chinese-blog",
   },
   {
     id: 4,
     title: "News Sentiment Analysis",
-    img: "/img/projects-news.png",
+    img: "/react-personal-site/public/img/projects-news.png",
     imgalt:"news project screenshot",
     link: "https://github.com/hellomomiji/news-sentiment-analysis",
   }
